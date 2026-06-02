202a0036373e07d8031595e161fbde20a256f0ac
Miriam <miriam101d@gmail.com>
2026-06-02 17:04:17 +0200

    xml 2

--- server/src/middleware/validation.middleware.ts
+++ server/src/middleware/validation.middleware.ts
@@ -1,24 +1,36 @@
-import { NextFunction,Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { create } from "xmlbuilder2";
 import { AnyZodObject } from "zod";
 
-export const validate=(schema:AnyZodObject)=>async(req:Request, res:Response,next:NextFunction)=>{
-    const result=await schema.safeParseAsync({
-        body:req.body,
-        query:req.query,
-        params:req.params
+export const validate = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+    const result = await schema.safeParseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params
     });
 
-    if(result.success){
+    if (result.success) {
         return next();
-    }else{
-        return res.status(400).json({
-            message:"Validation failed",
-            details: result.error.issues.map((issue)=>{
-                return {
+    } else {
+        if (req.headers["accept"] == "application/xml") {
+            const root=create().ele("errors",{message:"Validation failed"});
+            result.error.issues.forEach((issue)=>{
+                root.ele("error",{
                     path: issue.path.join(": "),
                     message: issue.message
-                }
+                })
             })
-        })
+        } else {
+
+            return res.status(400).json({
+                message: "Validation failed",
+                details: result.error.issues.map((issue) => {
+                    return {
+                        path: issue.path.join(": "),
+                        message: issue.message
+                    }
+                })
+            })
+        }
     }
 }
