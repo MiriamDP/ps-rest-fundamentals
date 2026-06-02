961b0f01c92bc000dff147181f2383b95b0f5359
Miriam <miriam101d@gmail.com>
2026-06-02 16:01:53 +0200

    get endpoints

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,31 @@
 import express from "express";
+import { getCustomerDetail, getCustomers, searchCustomers } from "./customers.service";
+import { getOrdersForCustomer } from "../orders/orders.service";
 
 export const customersRouter = express.Router();
+
+customersRouter.get("/",async(req,res)=>{
+  const customers=await getCustomers();
+  res.json(customers);
+});
+
+customersRouter.get("/:id",async(req,res)=>{
+  const id=req.params.id;
+  const customer=await getCustomerDetail(id);
+  if(customer!=null){
+    res.json(customer);
+  }else{
+    res.status(404).json({message:"Customer not found"});
+  }
+});
+
+customersRouter.get("/:id/orders", async(req,res)=>{
+    const id=req.params.id;
+    const orders=await getOrdersForCustomer(id);
+    res.json(orders);
+});
+customersRouter.get("/search/:query", async(req,res)=>{
+    const query=req.params.query;
+    const customer=await searchCustomers(query);
+    res.json(customer);
+});
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,31 @@
 import express from "express";
+import { getOrderDetail, getOrders } from "./orders.service";
 
 export const ordersRouter = express.Router();
+
+ordersRouter.get("/", async (req, res) => {
+    const query = req.query;
+    const take = query.take;
+    const skip = query.skip;
+
+
+    if(take && typeof take==="string" && parseInt(take)>0 && skip && typeof skip==="string" && parseInt(skip)>-1){
+        const orders = await getOrders(parseInt(skip),parseInt(take));
+        res.json(orders);
+    }else{
+        res.status(404).json({
+            message: "Take and skip are required"+"Take >0 y skip>-1"
+        });
+    }
+
+});
+
+ordersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const order = await getOrderDetail(id);
+    if (order != null) {
+        res.json(order);
+    } else {
+        res.status(404).json({ message: "Order not found" });
+    }
+});
